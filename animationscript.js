30e624f3b15299f30af4f1ea4d2358ed44101e29
programmeervid <78315156+programmeervid@users.noreply.github.com>
2021-03-31 16:08:48 +0200

    custom ease function

--- animationscript.js
+++ animationscript.js
@@ -8,5 +8,11 @@ function stopAnimation() {
     $('#timeline').stop();
     clearInterval(interval);
 }
-$("#timeline").animate({value: "1000"}, 8000, "easeOutQuad", function() {clearInterval(interval);});
+jQuery.easing["easeCustom"] = function(x, t, b, c, d) {
+    var ts=(t/=d)*t;
+    var tc=ts*t;
+    return b+c*(-4*tc*ts + 15*ts*ts + -20*tc + 10*ts);
+};
+// To use it: $('#elem').animate({width:200},'slow','easeInOutQuad');
+$("#timeline").animate({value: "1000"}, 6000, "easeCustom" /*"easeOutSine"*/ /*"easeOutQuad"*/, function() {clearInterval(interval);});
 interval = setInterval(updateMap,50);
